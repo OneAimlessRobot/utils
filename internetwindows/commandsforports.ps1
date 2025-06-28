06198a743c456cfa4d253d8147643d6b0c2d341a
OneAimlessRobot <adri.campos.valente@gmail.com>
2025-06-28 19:38:52 +0100

    Stuff to initialize git

--- internetwindows/commandsforports.ps1
+++ internetwindows/commandsforports.ps1
@@ -1,9 +1,5 @@
 
 
-New-NetFirewallRule -DisplayName 'Custom Inbound emotion streamer' -Profile @('Domain', 'Private') -Direction Inbound -Action Allow -Protocol TCP -LocalPort 30000-60000
+New-NetFirewallRule -DisplayName 'Custom Inbound emotion streamer' -Profile @('Domain', 'Private') -Direction Inbound -Action Allow -Protocol TCP -LocalPort 8001
 
-New-NetFirewallRule -DisplayName 'Custom Inbound emotion streamer' -Profile @('Domain', 'Private') -Direction Outbound -Action Allow -Protocol TCP -LocalPort 30000-60000
-
-New-NetFirewallRule -DisplayName 'Custom Inbound emotion streamer' -Profile @('Domain', 'Private') -Direction Inbound -Action Allow -Protocol UDP -LocalPort 30000-60000
-
-New-NetFirewallRule -DisplayName 'Custom Inbound emotion streamer' -Profile @('Domain', 'Private') -Direction Outbound -Action Allow -Protocol UDP -LocalPort 30000-60000
+New-NetFirewallRule -DisplayName 'Custom Outbound emotion streamer' -Profile @('Domain', 'Private') -Direction Outbound -Action Allow -Protocol TCP -LocalPort 8001
